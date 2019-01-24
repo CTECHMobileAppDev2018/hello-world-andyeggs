a96a8ceb00d69385b1a1daffa5604796897e5720
Andy Eggebraaten <andyjohneggs@gmail.com>
2019-01-24 08:53:03 -0600

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,10 +2,10 @@
 // You can test it in Dr. Java or in repl.it
 // If you get stuck, Google is your friend
 
-class Main {
+class HelloWorld {
   public static void main(String[] args) {
   
-    // Put your code here that prints out "Hello, world!"
+    System.out.println("Hello, world!");
     
   }
 }
